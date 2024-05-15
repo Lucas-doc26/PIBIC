c250a2197230581446af26a1869ff081c84d73ee
Lucas Cunha <lucaas.doc26@gmail.com>
2024-05-14 21:39:19 -0300

    Atualização nos comentário e explicações dos códigos

--- PIBIC/CNN-estacionamento/grafico_resultados.py
+++ PIBIC/CNN-estacionamento/grafico_resultados.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 
-# Dados dos modelos e testes
 modelos_testes = [
     "Dropout(25%, 25%, 50%) + Kernel regularizer",
     "Dropout(25%, 25%) + Kernel regularizer",
@@ -12,7 +11,7 @@ modelos_testes = [
     "Sem ajustes de Dropout e Kernel"
 ]
 
-# Acurácias correspondentes
+# Accuracy
 accuracies = [
     [0.999, 0.932, 0.62],
     [0.997, 0.931, 0.81],
@@ -24,44 +23,30 @@ accuracies = [
     [0.996, 0.948, 0.581]
 ]
 
-# Organizando os dados em listas separadas por modelo e teste
+# Organizando os dados em listas separadas
 num_modelos_testes = len(modelos_testes)
 num_acuracias = len(accuracies[0])
 num_barras = len(modelos_testes)
 
-# Largura das barras
 largura_barra = 0.2
-
-# Posições das barras
 posicoes = range(1, num_barras + 1)
 
-# Mapeamento das acurácias para os nomes dos modelos
 nome_modelo = {
     0: "PUCPR",
     1: "UFPR04",
     2: "UFPR05"
 }
 
-# Criando o gráfico
 plt.figure(figsize=(12, 8))
-
-# Iterando sobre as acurácias e plotando as barras para cada modelo e teste
 for i in range(num_acuracias):
     posicoes_deslocadas = [x + i * largura_barra for x in posicoes]
     plt.bar(posicoes_deslocadas, [accuracies[j][i] for j in range(num_modelos_testes)], width=largura_barra, label=f'{nome_modelo[i]}')
-    # Adicionando os valores das acurácias em cada barra
     for x, y in zip(posicoes_deslocadas, [accuracies[j][i] for j in range(num_modelos_testes)]):
         plt.text(x, y, f'{y:.3f}', ha='center', va='bottom')
-
-# Adicionando rótulos e título
 plt.xlabel('Ajustes')
 plt.ylabel('Accuracy')
 plt.title('Comparação de accuracy entre os ajustes')
 plt.xticks(posicoes, modelos_testes, rotation=45, ha='right')
-
-# Movendo a legenda para a parte inferior direita
 plt.legend(loc='lower right')
-
-# Exibindo o gráfico
 plt.tight_layout()
 plt.show()
--- PIBIC/CNN-estacionamento/segmentando_dataset.py
+++ PIBIC/CNN-estacionamento/segmentando_dataset.py
@@ -2,17 +2,14 @@ import pandas as pd
 import os
 import random
 
-# Lista de locais de interesse (substitua com os locais desejados)
 locais = ['PUC', 'UFPR04', 'UFPR05']
 
-# Definir limites por local
 limites = {
     'PUC': 5000,
     'UFPR04': 1000,
     'UFPR05': 1000
 }
 
-# Percorra cada local de interesse
 for local in locais:
     sample_dir = os.path.join(
         r'C:\Users\lucaa\OneDrive\Área de Trabalho\pythonProject\PIBIC\CNN-estacionamento\datasets\PKLot\PKLot\PKLotSegmented',
@@ -22,7 +19,6 @@ for local in locais:
     classes = []
     todos_arquivos = []
 
-    # Percorra as classes 'ocupada' e 'vaga'
     for class_dir in ['ocupada', 'vaga']:
         # Caminho completo para o subdiretório da classe
         full_class_dir = os.path.join(sample_dir, class_dir)
@@ -33,7 +29,6 @@ for local in locais:
             for file in os.listdir(full_class_dir):
                 # Verifique se o arquivo termina com '.jpg'
                 if file.endswith('.jpg'):
-                    # Adicione o caminho da imagem e a classe correspondente à lista de todos os arquivos
                     todos_arquivos.append((os.path.join(full_class_dir, file), class_dir))
         else:
             print(f'Diretório não encontrado: {full_class_dir}')
@@ -41,16 +36,11 @@ for local in locais:
     # Embaralhe a lista de arquivos de forma aleatória
     random.shuffle(todos_arquivos)
 
-    # Obtenha o limite de arquivos para o local atual
     limite_arquivos = limites[local]
 
-    # Percorra a lista embaralhada e adicione os arquivos às listas 'caminhos_imagem' e 'classes'
     for i, (file_path, class_dir) in enumerate(todos_arquivos):
         if i < limite_arquivos:
-            # Adicione o caminho da imagem à lista
             caminhos_imagem.append(file_path)
-
-            # Adicione a classe correspondente à lista
             classes.append(class_dir)
         else:
             # Se o limite for atingido, interrompa o loop
@@ -62,10 +52,8 @@ for local in locais:
         'classe': classes
     })
 
-    # Salve o DataFrame como um arquivo CSV com um nome distinto para cada local
     df.to_csv(f'datasets/df_{local}.csv', index=False)
 
-    # Exiba as primeiras linhas do DataFrame para verificar
     print(f'DataFrame do local {local}:')
     print(df.head())
     print('\n')
